9a1fe0fef3eb73e9604df3bbd37c8d581d27aadf
Андрей Бердников <tsofistgudmen@gmail.com>
2022-02-20 03:22:57 +0300

    fix: remove lib from repo

--- lib/consts.d.ts
+++
@@ -1,5 +0,0 @@
-/** VendorID for Silicon Labs */
-export declare const USBVendorId = 4292;
-export declare const CommandStartByte = 165;
-export declare const CommandSecondByte = 90;
-export declare const ResponseHeaderLength = 7;
--- lib/util.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function raise(message: string): never;
-export declare function delay(ms?: number): Promise<void>;
-export declare function round(val: number): number;
